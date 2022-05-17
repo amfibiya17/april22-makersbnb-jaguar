3713393c73a864ec74d5f5e29664a6c6ea86dc42
alexandrospapagiannis <alpa31291@gmail.com>
2022-05-17 17:23:11 +0100

    Completing the setup for /list route

--- spec/features/list/list_spec.rb
+++ spec/features/list/list_spec.rb
@@ -5,6 +5,8 @@ feature 'List home' do
     fill_in :name, with: "Name"
     fill_in :description, with: "A lovely apartment"
     fill_in :price, with: "30"
+    fill_in :start_date, with: "2022/05/18"
+    fill_in :end_date, with: "2022/05/21"
     click_button :Submit
     expect(current_path).to eq '/selection' 
   end
