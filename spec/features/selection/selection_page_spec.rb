37a814fa39ae59e8833af0047670fe711ed18c03
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-05-17 10:35:56 +0100

    page is ready, needs to add tests

--- spec/features/selection/selection_page_spec.rb
+++ spec/features/selection/selection_page_spec.rb
@@ -1,11 +1,11 @@
 feature 'setting up the page' do
-  scenario 'checking behaviour' do
+  scenario 'checking behavior' do
     visit('/selection')
-    expect(page).to have_link("Homes", href: '/homes')
-    expect(page).to have_link("List", href: '/list')
-    expect(page).to have_link("Profile", href: 'profile')
-    expect(page).to have_link("Sign out", href: '/login')
-    expect(page).to have_button("Rent", href: '/homes')
-    expect(page).to have_button("List", href: '/list')
+    expect(page).to have_content "HOMES"
+    expect(page).to have_content "LIST A HOME"
+    expect(page).to have_content "PROFILE"
+    expect(page).to have_content "SIGN OUT"
+    expect(page).to have_content "RENT HOME"
+    expect(page).to have_content "LIST HOME"
   end
 end
