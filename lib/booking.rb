3d4ddf83aa51534f44664323de560ee986f8a266
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-05-18 17:28:47 +0100

    writing class Booking - trying to get a boollean from the database query, not yet successful

--- lib/booking.rb
+++ lib/booking.rb
@@ -3,8 +3,13 @@ class Booking
   def make(user, home, night)
     p user
     p home[0]['home_name']
-    p night
-    
+    # p night
+    night = '2022-06-19'
+    connection = PG.connect(dbname: "makersbnb_test")
+
+    available = connection.exec("SELECT * FROM available WHERE home_id = #{home[0]['id']} AND night = '#{night}';")
+    available[0]
+    # !available.include? ("home_id") ? true : false
   end
 
 end
--- spec/unit/booking_spec.rb
+++ spec/unit/booking_spec.rb
@@ -12,7 +12,7 @@ describe Booking do
     end
     it 'can make a booking with a user and a home and a date' do
       home_name = 'Waterloo 25'
-      night = '2022-05-17'
+      night = '2022-05-16'
       #create a user using the user class (already written)
       user = User.create(username: 'test_name', password: '12345678', email: 'test@email.com')
       #connect to the test table
@@ -22,7 +22,7 @@ describe Booking do
       ["#{home_name}", 'This is the description', 500, 1])
       #insert some data into the available table
       connection.exec("INSERT INTO available (home_id, night) VALUES($1, $2);", ["#{home[0]['id']}", night])
-      expect(subject.make(user, home, night)).to eq night
+      expect(subject.make(user, home, night)).to eq true
     end
   end
 
