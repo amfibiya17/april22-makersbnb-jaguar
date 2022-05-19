1c97572c0542614cb8343c11577a07bfeb6d6d3e
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-05-19 16:54:26 +0100

    updated bookings and requests databases to use renter_id and owner_id

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,5 +3,5 @@ require 'pg'
 def setup_test_database
   connection = PG.connect(dbname: 'makersbnb_test')
   
-  connection.exec("TRUNCATE users, homes, bookings, available RESTART IDENTITY;")
+  connection.exec("TRUNCATE users, homes, bookings, available, requests RESTART IDENTITY;")
 end
