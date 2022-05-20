c1e1a54eb96952083624435bc1bfee9effaaf689
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-05-20 12:21:01 +0100

    connecting to both databases, Booking.approved removes from requests table, Booking.request_all returns contents of requests table

--- spec/unit/booking_spec.rb
+++ spec/unit/booking_spec.rb
@@ -52,6 +52,7 @@ describe Booking do
       Booking.approved(user, home, night)
       expect(Booking.check(home, night)).to eq true
       expect(Booking.available?(home, night)).to eq false
+      expect(Booking.request_check?(home, night)).to eq false
     end
   end
   describe '.request' do 
@@ -70,7 +71,37 @@ describe Booking do
       connection.exec("INSERT INTO available (home_id, night) VALUES($1, $2);", ["#{home[0]['id']}", night])
       expect(Booking.available?(home, night)).to eq true
       Booking.request(user, home, night)
-      expect(Booking.owner_requests(user.id)).to eq true
+      expect(Booking.request_check?(home, night)).to eq true
     end
   end
+
+  describe '.request_all' do
+    it 'returns all items in requests table' do
+      #adds it to a table that we've not made yet
+      home_name = 'Waterloo 25'
+      home_name2 = 'Home 2'
+      night = '2022-05-16'
+      night2 = '2022-08-20'
+      #create a user using the user class (already written)
+      user = User.create(username: 'test_name', password: '12345678', email: 'test@email.com')
+      user2 = User.create(username: 'test_name2', password: '23456789', email: 'test2@email.com')
+      #connect to the test table
+      connection = PG.connect(dbname: "makersbnb_test")
+      #put some data into the homes table 
+      home = connection.exec("INSERT INTO homes (home_name, description, price, owner_id) VALUES($1, $2, $3, $4) RETURNING id, home_name;", 
+      ["#{home_name}", 'This is the description', 500, 1])
+      home2 = connection.exec("INSERT INTO homes (home_name, description, price, owner_id) VALUES($1, $2, $3, $4) RETURNING id, home_name;", 
+      ["#{home_name2}", 'This is the description', 500, 1])
+      #insert some data into the available table
+      connection.exec("INSERT INTO available (home_id, night) VALUES($1, $2);", ["#{home[0]['id']}", night])
+      connection.exec("INSERT INTO available (home_id, night) VALUES($1, $2);", ["#{home2[0]['id']}", night2])
+      expect(Booking.available?(home, night)).to eq true
+      expect(Booking.available?(home2, night2)).to eq true
+      Booking.request(user, home, night)
+      Booking.request(user2, home2, night2)
+      Booking.request_all
+      expect(Booking.request_all[0]['home_id']).to include home[0]['id']
+    end
+  end
+
 end
